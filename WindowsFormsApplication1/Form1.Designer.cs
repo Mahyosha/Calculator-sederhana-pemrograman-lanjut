0d740d1e9642613c76edf56defde14a9763fba18
Mahyosha <Mahyosha.muhammad@students.amikom.ac.id>
2018-09-25 15:43:56 +0700

    Tugas Pemrograman Lanjut
    Tugas 1, membuat calculator consol mode

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.NamaComoBox = new System.Windows.Forms.ComboBox();
+            this.NilaiA = new System.Windows.Forms.TextBox();
+            this.NilaiB = new System.Windows.Forms.TextBox();
+            this.txtHasil = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Operator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nilai A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nilai B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Hasil";
+            // 
+            // NamaComoBox
+            // 
+            this.NamaComoBox.FormattingEnabled = true;
+            this.NamaComoBox.Items.AddRange(new object[] {
+            "Penjumlahan",
+            "Pengurangan",
+            "Perkalian",
+            "Pembagian"});
+            this.NamaComoBox.Location = new System.Drawing.Point(87, 2);
+            this.NamaComoBox.Name = "NamaComoBox";
+            this.NamaComoBox.Size = new System.Drawing.Size(121, 24);
+            this.NamaComoBox.TabIndex = 4;
+            // 
+            // NilaiA
+            // 
+            this.NilaiA.Location = new System.Drawing.Point(87, 39);
+            this.NilaiA.Name = "NilaiA";
+            this.NilaiA.Size = new System.Drawing.Size(100, 22);
+            this.NilaiA.TabIndex = 5;
+            // 
+            // NilaiB
+            // 
+            this.NilaiB.Location = new System.Drawing.Point(87, 74);
+            this.NilaiB.Name = "NilaiB";
+            this.NilaiB.Size = new System.Drawing.Size(100, 22);
+            this.NilaiB.TabIndex = 6;
+            // 
+            // txtHasil
+            // 
+            this.txtHasil.Location = new System.Drawing.Point(87, 108);
+            this.txtHasil.Name = "txtHasil";
+            this.txtHasil.Size = new System.Drawing.Size(100, 22);
+            this.txtHasil.TabIndex = 7;
+            this.txtHasil.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Mencari";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtHasil);
+            this.Controls.Add(this.NilaiB);
+            this.Controls.Add(this.NilaiA);
+            this.Controls.Add(this.NamaComoBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox NamaComoBox;
+        private System.Windows.Forms.TextBox NilaiA;
+        private System.Windows.Forms.TextBox NilaiB;
+        private System.Windows.Forms.TextBox txtHasil;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Nilai_A, Nilai_B, Hasil;
+
+            if (NamaComoBox.SelectedIndex == 0)
+            {
+                Nilai_A = double.Parse(NilaiA.Text);
+                Nilai_B = double.Parse(NilaiB.Text);
+                Hasil = Nilai_A + Nilai_B;
+                txtHasil.Text = Hasil.ToString();
+            }
+            else if (NamaComoBox.SelectedIndex == 1)
+            {
+                Nilai_A = double.Parse(NilaiA.Text);
+                Nilai_B = double.Parse(NilaiB.Text);
+                Hasil = Nilai_A - Nilai_B;
+                txtHasil.Text = Hasil.ToString();
+            }
+
+            else if (NamaComoBox.SelectedIndex == 2)
+            {
+                Nilai_A = double.Parse(NilaiA.Text);
+                Nilai_B = double.Parse(NilaiB.Text);
+                Hasil = Nilai_A * Nilai_B;
+                txtHasil.Text = Hasil.ToString();
+            }
+
+            else if (NamaComoBox.SelectedIndex == 3)
+            {
+                Nilai_A = double.Parse(NilaiA.Text);
+                Nilai_B = double.Parse(NilaiB.Text);
+                Hasil = Nilai_A / Nilai_B;
+                txtHasil.Text = Hasil.ToString();
+            }
+        }
+    }
+}
